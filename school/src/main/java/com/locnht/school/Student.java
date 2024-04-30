490c66737832d6b957925162cc2532bb3ffccd16
locnht-it <locnht.it@gmail.com>
2024-05-01 01:42:21 +0700

    update the first version

---
+++ school/src/main/java/com/locnht/school/Student.java
@@ -0,0 +1,19 @@
+package com.locnht.school;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Student {
+
+    private String firstname;
+
+    private String lastname;
+
+    private String email;
+}
---
+++ student/src/main/java/com/locnht/student/Student.java
@@ -0,0 +1,26 @@
+package com.locnht.student;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String firstname;
+
+    private String lastname;
+
+    private String email;
+
+    private Integer schoolId;
+}
